66b9ccdef2b0549b40d06312cc4e92cd0ed1aa1d
an-alch3mist <kaeya000@gmail.com>
2025-11-16 21:56:57 +0600

    18

--- Scripts/DoorSystem/SimpleIDoor/SimpleDoorAnimEventForwarder.cs
+++ Scripts/DoorSystem/SimpleIDoor/SimpleDoorAnimEventForwarder.cs
@@ -4,21 +4,24 @@ using System.Collections.Generic;
 
 using SPACE_UTIL;
 
-public class SimpleDoorAnimEventForwarder : MonoBehaviour
+namespace SPACE_GAME_1
 {
-	IDoor Idoor;
-	private void Awake()
+	public class SimpleDoorAnimEventForwarder : MonoBehaviour
 	{
-		Debug.Log(C.method(this));
-		this.Idoor = this.GetComponent<IDoor>();
-		Debug.Log(this.Idoor);
+
+		[SerializeField] DoorBase _doorBase;
+		private void Awake()
+		{
+			Debug.Log(C.method(this));
+		}
+
+		public void AEOnDoorOpenComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.DoorOpeningComplete);
+		public void AEOnDoorCloseComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.DoorClosingComplete);
+
+		public void AEOnInsideLockComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.InsideLockingComplete);
+		public void AEOnInsideUnlockComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.InsideUnlockingComplete);
+		public void AEOnOutsideLockComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.OutsideLockingComplete);
+		public void AEOnOutsideUnlockComplete() => this._doorBase.OnAnimationComplete(AnimationEventType.OutsideUnlockingComplete);
 	}
 
-	public void AEOnDoorOpenComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.DoorOpeningComplete);
-	public void AEOnDoorCloseComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.DoorClosingComplete);
-
-	public void AEOnInsideLockComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.InsideLockingComplete);
-	public void AEOnInsideUnlockComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.InsideUnlockingComplete);
-	public void AEOnOutsideLockComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.OutsideLockingComplete);
-	public void AEOnOutsideUnlockComplete() => this.Idoor.OnAnimationComplete(AnimationEventType.OutsideUnlockingComplete);
 }
